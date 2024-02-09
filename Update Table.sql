d8ff7b2e3a07027d78ef6d6d971ccc1ab1c0e601
Jun Anthony <151007316+janthony25@users.noreply.github.com>
2024-02-10 07:26:29 +1300

    Add files via upload

---
+++ Update Table.sql
@@ -0,0 +1,13 @@
+Use Company
+Go
+
+
+/*
+Update tableEmployees
+set EmployeeName ='John'
+Where EmployeeID=1005
+*/
+
+Update tableEmployees
+set EmployeeName='Eric', Phone= 12345676
+where EmployeeID = 1005
