92c4c890080fbfa1816fcdeeb9d6e10894c8711c
Jun Anthony <151007316+janthony25@users.noreply.github.com>
2024-02-10 07:14:10 +1300

    Add files via upload

---
+++ Insert to Table.sql
@@ -0,0 +1,13 @@
+Use Company
+Go
+
+/* *********** INSERTING VALUES WITH COLUMN *********
+
+Insert Into tableEmployees 
+(EmployeeID, EmployeeName, Phone, DepID)
+values(1005, 'Steve', 44889834, 1)
+
+*/
+
+Insert Into tableEmployees
+values(1006,'Tom', 44889877, 4)
